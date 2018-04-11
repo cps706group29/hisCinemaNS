60a24b32233fbd1578de43071d4d58ad98c39392
Philam18 <philiplam818@gmail.com>
2018-04-11 09:03:11 -0400

    update

---
+++ HisCinemaNS.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.*;
+
+public class HisCinemaNS{
+  ArrayList<ResourceRecord> records = new ArrayList<ResourceRecord>();
+
+
+  public static void main(String argv[]) throws Exception{
+
+    records.add(new ResourceRecord("video.hiscinema.com", "herCDN.com",     "R"));
+    records.add(new ResourceRecord("herCDN.com",          "www.herCDN.com", "CN"));
+    records.add(new ResourceRecord("www.herCDN.com",      "127.0.0.1",      "A"));
+
+
+    DatagramSocket serverSocket = new DatagramSocket(40282);
+    byte[] receiveData = new byte[1024];
+    byte[] sendData = new byte[1024];
+
+    // HisCinemaNS runs infinitely, listens for any incoming DNS queries
+    while(true){
+      // incoming packet arrives at the socket
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      serverSocket.receive(receivePacket);
+
+      String requestURL = new String(receivePacket.getData());
+      System.out.println("Incoming Message: " + requestURL);
+
+      // Prepare a response
+      InetAddress IPAddress = receivePacket.getAddress();
+      int port = receivePacket.getPort();
+      ///////////////////////// Take requestURL from UDP message, and resolve it ///////////////////////
+      
+
+      //////////////////////////////////////////////////////////////////////////////////////
+      String response = requestURL;
+      sendData = response.getBytes();
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+
+      // Send the Response
+      serverSocket.send(sendPacket);
+    }
+  }
+
+
+
+}
---
+++ ResourceRecord.java
@@ -0,0 +1,16 @@
+public class ResourceRecord{
+  public String name;
+  public String value;
+  public String type;
+
+  public ResourceRecord(String name, String value, String type){
+    this.name = name;
+    this.value = value;
+    this.type = type;
+  }
+
+  public String resolve(){
+    return this.value;
+  }
+
+}
